31385e44ee10de065e814e759b636e396a6e24a7
Mohammed El Mahdy <m5z.mohammed@gmail.com>
2023-01-02 19:33:53 +0200

    law fix

--- resources/views/website/pages/laws.blade.php
+++ resources/views/website/pages/laws.blade.php
@@ -17,8 +17,8 @@
                         <div class="col-lg-4 col-md-6 col-sm-12">
                             <a href="{{ $law->attachment_pdf }}" target="_blank">
                                 <div class="card m-3 rounded-4 shadow-custom p-0">
-                                    <img src="{{ $information_app->logo_original_path }}" class="p-5"
-                                        style="border-radius: 16px 16px 0 0;" alt="...">
+                                    <img src="{{ $information_app->translate(App::getLocale())->logo_original_path }}"
+                                        class="p-5" style="border-radius: 16px 16px 0 0;" alt="...">
                                     <div class=" text-white  text-center p-3"
                                         style="background-color: #00113D;border-radius: 0 0 16px 16px;">
                                         <p class="fw-bold fs-5 mb-1">
@@ -31,6 +31,7 @@
                     @empty
                     @endforelse
                 </div>
+                {{ $laws->links() }}
             </div>
         </div>
     </section>
