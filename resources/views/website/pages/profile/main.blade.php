b4f4575062837fa6f4669fc9763436740010d5b1
Mohammed El Mahdy <m5z.mohammed@gmail.com>
2023-01-08 15:23:59 +0200

    Main Profile fix

--- resources/views/website/pages/profile/main.blade.php
+++ resources/views/website/pages/profile/main.blade.php
@@ -4,43 +4,43 @@
 
 @section('content')
 
-@include('website.layout._header_page', [
-'title' => 'Profile',
-'pageName' => 'Profile',
-])
-<section class="bg-content-custom p-custom">
-    <div class="row">
-        <div class="col-2">
-            @include('website.pages.profile._aside')
-        </div>
-        <div class="col-10">
-            <div class="shadow-custom rounded-4  bg-white ">
-                @include('website.inc._error')
-                @include('flash::message')
-                <div class="row gx-0 p-3">
-                    <div class="col-lg-12 col-md-12 py-3 px-2">
-                        <i class="fa-sharp fa-solid fa-circle-user fa-lg text-info d-inline"></i>
-                        <p class="firstWordInfo d-inline">
-                            My Account
-                        </p>
-                    </div>
-                    <div class="row my-3 p-3">
-                        <div class="col-5">Full Name :</div>
-                        <div class="col-7">{{ $user->full_name }}</div>
-                    </div>
-                    <div class="row my-3 p-3">
-                        <div class="col-5">Email :</div>
-                        <div class="col-7">{{ $user->email }}</div>
-                    </div>
-                    <div class="row my-3 p-3">
-                        <div class="col-5">Phone :</div>
-                        <div class="col-7">{{ $user->country->code??'' }} {{ $user->phone }}</div>
-                    </div>
-                    <div class="row my-3 p-3">
-                        <div class="col-5">Account Created At :</div>
-                        <div class="col-7">{{ $user->created_at }}</div>
-                    </div>
-                    <div class="row my-3 p-3">
+    @include('website.layout._header_page', [
+        'title' => 'Profile',
+        'pageName' => 'Profile',
+    ])
+    <section class="bg-content-custom p-custom">
+        <div class="row">
+            <div class="col-2">
+                @include('website.pages.profile._aside')
+            </div>
+            <div class="col-10">
+                <div class="shadow-custom rounded-4  bg-white ">
+                    @include('website.inc._error')
+                    @include('flash::message')
+                    <div class="row gx-0 p-3">
+                        <div class="col-lg-12 col-md-12 py-3 px-2">
+                            <i class="fa-sharp fa-solid fa-circle-user fa-lg text-info d-inline"></i>
+                            <p class="firstWordInfo d-inline">
+                                My Account
+                            </p>
+                        </div>
+                        <div class="row my-3 p-3">
+                            <div class="col-5">Full Name :</div>
+                            <div class="col-7">{{ $user->full_name }}</div>
+                        </div>
+                        <div class="row my-3 p-3">
+                            <div class="col-5">Email :</div>
+                            <div class="col-7">{{ $user->email }}</div>
+                        </div>
+                        <div class="row my-3 p-3">
+                            <div class="col-5">Phone :</div>
+                            <div class="col-7">{{ $user->country->code ?? '' }} {{ $user->phone }}</div>
+                        </div>
+                        <div class="row my-3 p-3">
+                            <div class="col-5">Account Created At :</div>
+                            <div class="col-7">{{ $user->created_at }}</div>
+                        </div>
+                        {{-- <div class="row my-3 p-3">
                         <div class="col-5">Request to Advisor :</div>
                         <div class="col-6">0</div>
                         <div class="col-1">
@@ -93,13 +93,13 @@
                                 <i class="fa-sharp fa-solid fa-circle-plus fa-lg text-info"></i>
                             </a>
                         </div>
-                    </div>
+                    </div> --}}
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-</section>
+    </section>
 
 @endsection
