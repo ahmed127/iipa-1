b5e33ec940bd2947d48d681a08b3768e491486c8
Ahmed EL-Gammal <ahmedmabdalla91@gmail.com>
2023-05-14 21:03:19 +0300

    front

--- resources/views/website/pages/profile/information.blade.php
+++ resources/views/website/pages/profile/information.blade.php
@@ -4,91 +4,96 @@
 
 @section('content')
 
-@include('website.layout._header_page', [
-'title' => 'Update Information',
-'pageName' => 'Update Information',
-])
-<section class="bg-content-custom p-custom">
-    <div class="row">
-        <div class="col-2">
-            @include('website.pages.profile._aside')
-        </div>
-        <div class="col-10">
-            @include('website.inc._error')
-            @include('flash::message')
-            <div class="shadow-custom rounded-4  bg-white ">
+    @include('website.layout._header_page', [
+        'title' => __('lang.update_information'),
+        'pageName' => __('lang.update_information'),
+    ])
+    <section class="bg-content-custom p-custom">
+        <div class="row justify-content-center">
+            <div class="col-xl-2 col-md-10 flex-md-column my-3">
+                @include('website.pages.profile._aside')
+            </div>
+            <div class="col-xs-12 col-sm-10">
                 @include('website.inc._error')
                 @include('flash::message')
-                <form action="{{ route('website.update_information_post') }}" method="POST">
-                    @csrf
-                    <div class="row gx-0 p-3 w-75 m-auto">
-                        <div class="col-lg-9 col-md-9 py-5 px-2">
-                            <i class="fa-sharp fa-solid fa-circle-user fa-lg fs-3 text-info d-inline"></i>
-                            <h3 class="firstWordInfo d-inline">
-                                @lang('lang.update_information')
-                            </h3>
-                        </div>
-                        <div class="col-lg-3 col-md-3 py-5 px-2 text-end">
-                            <a href="{{ route('website.home') }}"
-                                class="btn btn-light rounded-4 px-5 py-2 shadow-custom ">
-                                <i class="fa-sharp fa-solid fa-circle-xmark"></i>
-                                @lang('lang.cancel')
-                            </a>
-                        </div>
-                        <div class="col-lg-12 col-md-12 px-2 mb-3 fs-6 d-flex">
-                            <label for="full_name" class="form-label px-1 w-25 m-auto">
-                                <i class="fa-solid fa-arrow-left text-secondary opacity-50 fa-sm"></i>
-                                @lang('lang.full_name') :
-                                <span class="text-danger">*</span>
-                            </label>
-                            <div class="input-group w-75">
-                                <input type="text" class="form-control border text-start py-3" id="full_name"
-                                    name="full_name" value="{{ $user->full_name }}" required>
+                <div class="shadow-custom rounded-4  bg-white ">
+                    @include('website.inc._error')
+                    @include('flash::message')
+                    <form action="{{ route('website.update_information_post') }}" method="POST">
+                        @csrf
+                        <div class="row gx-0 p-3 m-auto">
+                            <div class="col-lg-9 col-md-9 py-3 col-sm-12">
+                                <i class="fa-sharp fa-solid fa-circle-user fa-lg fs-3 text-info d-inline"></i>
+                                <h4 class="firstWordInfo d-inline">
+                                    @lang('lang.update_information')
+                                </h4>
                             </div>
-                        </div>
-                        <div class="col-lg-12 col-md-12 px-2 mb-3 fs-6 d-flex">
-                            <label for="email" class="form-label px-1 w-25 m-auto">
-                                <i class="fa-solid fa-arrow-left text-secondary opacity-50 fa-sm"></i>
-                                @lang('lang.email') :
-                                <span class="text-danger">*</span>
-                            </label>
-                            <div class="input-group w-75">
-                                <input type="email" class="form-control border text-start py-3" id="email" name="email"
-                                    value="{{ $user->email }}" required>
+                            <div class="col-lg-3 col-md-3 p-3 text-end">
+                                <a href="{{ route('website.home') }}"
+                                    class="btn btn-light rounded-4 p-2 w-100 shadow-custom ">
+                                    <i class="fa-sharp fa-solid fa-circle-xmark d-block"></i>
+                                    @lang('lang.cancel')
+                                </a>
                             </div>
-                        </div>
-                        <div class="col-lg-12 col-sm-12 col-md-6 px-2 mb-3 fs-6 d-flex">
-                            <label for="phone" class="form-label px-1 w-25 m-auto">
-                                <i class="fa-solid fa-arrow-left text-secondary opacity-50 fa-sm"></i>
-                                @lang('lang.phone') :
-                                <span class="text-danger">*</span>
-                            </label>
-                            <div class="input-group rounded-4 shadow-sm w-75">
-                                <label for="country_code">
-                                    {!! Form::select('country_code', $countryCodes, $user->country_code??null, ['class'
-                                    =>
-                                    'border
-                                    border-end-0 py-3
-                                    form-select bg-primary text-white', 'required'=> 'required']) !!}
+                            <div class="col-12 p-2">
+                                <label for="old_password" class="form-label px-2 m-auto">
+                                    <i class="fa-solid fa-arrow-left reversed text-secondary opacity-50 fa-sm"></i>
+                                    @lang('lang.full_name') :
+                                    <span class="text-danger">*</span>
                                 </label>
-                                <input type="text" name="phone" value="{{ $user->phone }}"
-                                    class="form-control border border-start-0 text-start py-3 " id="phone" required>
+                            </div>
+                            <div class="col-12 px-2 mb-3 fs-6 d-flex">
+                                <div class="input-group">
+                                    <input type="text" class="form-control border border-end-0 text-start py-3 rounded-4 shadow-sm"
+                                        id="old_password" value="{{ $user->full_name }}" name="old_password" required>
+                                </div>
+                            </div>
+                            <div class="col-12 p-2">
+                                <label for="old_password" class="form-label px-2 m-auto">
+                                    <i class="fa-solid fa-arrow-left reversed text-secondary opacity-50 fa-sm"></i>
+                                    @lang('lang.email') :
+                                    <span class="text-danger">*</span>
+                                </label>
+                            </div>
+                            <div class="col-12 px-2 mb-3 fs-6 d-flex">
+                                <div class="input-group">
+                                    <input type="email" class="form-control border text-start py-3 rounded-4 shadow-sm" id="email"
+                                        name="email" value="{{ $user->email }}" required>
+                                </div>
+                            </div>
+                            <div class="col-12 p-2">
+                                <label for="phone" class="form-label px-2 m-auto">
+                                    <i class="fa-solid fa-arrow-left text-secondary opacity-50 fa-sm"></i>
+                                    @lang('lang.phone') :
+                                    <span class="text-danger">*</span>
+                                </label>
+                            </div>
+                            <div class="col-12 px-2 mb-3 fs-6 d-flex">
+                                <div class="input-group rounded-4 shadow-sm ">
+                                    <input type="text" name="phone" value="{{ $user->phone }}"
+                                        class="form-control border border-end-0 text-start py-3 direction-input-rtl direction-input-ltr" id="phone" required>
+                                    <label for="country_code">
+                                        {!! Form::select('country_code', $countryCodes, $user->country_code ?? null, [
+                                            'class' => 'py-3 form-select bg-primary rounded-0 rounded-end shadow-sm text-white',
+                                            'required' => 'required',
+                                        ]) !!}
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-12 col-md-6 py-4 text-center">
+                                <button type="submit"
+                                    class="btn btn-primary rounded-4 py-3 w-100 shadow-custom">
+                                    <i class="fa-solid fa-circle-check"></i>
+                                    @lang('lang.save')
+                                </button>
+
                             </div>
                         </div>
-                        <div class="col-lg-12 col-sm-12 col-md-6 py-4 text-center">
-                            <button type="submit"
-                                class="btn btn-primary rounded-4 px-5 py-2 col-lg-3 col-sm-6 shadow-custom m-2">
-                                <i class="fa-solid fa-circle-check"></i>
-                                @lang('lang.save')
-                            </button>
-
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
 
-</section>
+    </section>
 
 @endsection
