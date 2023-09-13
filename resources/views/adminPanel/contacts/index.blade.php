66f9ecfd1d4193af9de80b8c509abffe1fbf462e
mohamed.ashry <mohamed.reda@safavisa.com>
2023-09-13 23:50:17 +0300

    .

--- resources/views/adminPanel/contacts/index.blade.php
+++ resources/views/adminPanel/contacts/index.blade.php
@@ -22,12 +22,8 @@
 
         <div class="card-body">
             @include('adminPanel.contacts.table')
-        </div>
-
-        <div class="card-body">
-            @include('adminPanel.companies.table')
             <div class="pull-right mr-3">
-                @include('coreui-templates::common.paginate', ['records' => $companies])
+                @include('coreui-templates::common.paginate', ['records' => $contacts])
 
             </div>
         </div>
